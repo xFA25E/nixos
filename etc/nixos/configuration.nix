1fd3eb4d752e6647f91ee09dce8219656071937a
xFA25E <55886566+xFA25E@users.noreply.github.com>
2021-02-05 19:45:13 +0100

    update

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -63,124 +63,57 @@
     keyMap = "dvorak";
   };
 
-  # Set your time zone.
   time.timeZone = "Europe/Rome";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    checkbashisms
-    dash
-    dejavu-fonts
-    dmenu
-    emacs
-    fd
-    feh
-    file
-    firefox
-    git
-    gnupg
-    hack-font
-    htop
-    iosevka
-    jq
-    ledger
-    leiningen
-    libfixposix
-    libreoffice-7.0.3.1
-    mpc
-    mpd
-    mpop
-    mpv
-    msmtp
-    mtpfs
-    mu
-    p7zip
-    pass-otp
-    password-store
-    pcre
-    pcre2
-    pinentry
-    pueue
-    pulsemixer
-    pwgen
-    qrencode
-    qtox
-    qutebrowser
-    ripgrep
-    rsync
-    rustup
-    sbcl
-    sdcv
-    shellcheck
-    simplescreenrecorder
-    sloccount
-    speedtest-cli
-    stalonetray
-    stow
-    sxiv
-    syncthing
-    telegram-desktop
-    transmission
-    ungoogled-chromium
-    woof
-    xclip
-    xz
-    youtube-dl
-    zip
+    checkbashisms dash dejavu_fonts dmenu emacs fd feh file firefox git gnupg
+    hack-font htop iosevka jq ledger leiningen libfixposix libreoffice-fresh
+    mpc_cli mpd mpop mpv msmtp mtpfs mu p7zip pass-otp pass pcre pcre2 pinentry
+    pueue pulsemixer pwgen qrencode qtox qutebrowser ripgrep rsync rustup sbcl
+    sdcv shellcheck simplescreenrecorder sloccount speedtest-cli stalonetray
+    stow sxiv syncthing tdesktop transmission ungoogled-chromium woof xclip xz
+    youtube-dl zip
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
     pinentryFlavor = "gnome3";
   };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
   # services.openssh.permitRootLogin = "yes";
 
-  # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.layout = "dvorak,ru";
   services.xserver.xkbVariant = ",ruu"
   services.xserver.xkbOptions = "ctrl:swapcaps,grp:shifts_toggle";
 
-  # Enable touchpad support.
   services.xserver.libinput.enable = true;
 
-  # Enable the KDE Desktop Environment.
   services.xserver.displayManager.startx.enable = true;
   services.xserver.displayManager.defaultSession = "none";
   services.xserver.windowManager.stumpwm.enable = true;
 
-  # users = let secrets = import ./secrets.nix;
-  #         in {
-  #           defaultUserShell = pkgs.zsh;
-  #           mutableUsers = false;
-  #           users.root.hashedPassword = secrets.root.hashedPassword;
-  #           extraUsers.adomas = {
-  #             hashedPassword = secrets.adomas.hashedPassword;
-  #             isNormalUser = true;
-  #             uid = 1000;
-  #             extraGroups = [ "video" "wheel" ];
-  #             shell = pkgs.zsh;
-  #           };
-  #         };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.val = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "audio" ];
-  };
+  # mkpasswd --method=sha-512 --stdin
+  users = let secrets = import ./secrets.nix;
+          in {
+            defaultUserShell = pkgs.dash;
+            mutableUsers = false;
+            users.root.hashedPassword = secrets.root.hashedPassword;
+            users.val = {
+              hashedPassword = secrets.val.hashedPassword;
+              isNormalUser = true;
+              uid = 1000;
+              extraGroups = [ "video" "wheel" "networkmanager" "audio"];
+              shell = pkgs.dash;
+              openssh.authorizedKeys.keys = [];
+              openssh.authorizedKeys.keyFiles = [];
+            };
+          };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -188,7 +121,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
+  system.stateVersion = "20.09"; # Did you read the comment?
 
   security.sudo.configFile = ''
     %wheel ALL=(ALL) ALL
---
+++ etc/nixos/secrets.nix
@@ -0,0 +1,4 @@
+{
+  root.hashedPassword = "";
+  val.hashedPassword = "";
+}
